af7e9791ce312322b77e9a14e76f3e1e4e8183fa
Yingcong Feng <FengY52@cardiff.ac.uk>
2025-04-14 22:03:33 +0100

    change method name 'addTopicName' to 'addTopicNameToSelector' that makes it more sense

--- script.js
+++ script.js
@@ -6,28 +6,31 @@ const select = document.getElementById("topic-select");
 const topicContainer = document.querySelector(".topic-container");
 
 function formatTime(time) {
-    return new Date(time).toLocaleTimeString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-    });
+  return new Date(time).toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
 }
 
 function formatDate(date) {
-    return new Date(date).toLocaleDateString("en-GB", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric"
-    })
+  return new Date(date).toLocaleDateString("en-GB", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 }
 
 function renderPrograms(programs) {
-    return programs
-        .map((program) => `
+  return programs
+    .map(
+      (program) => `
         <div class="program">
             <section class="program-title-type">
-                <div class="program-type" style="background: ${program.programType.type_colour}">
+                <div class="program-type" style="background: ${
+                  program.programType.type_colour
+                }">
                     ${program.programType.type}
                 </div>
                 <h3>${program.title}</h3>
@@ -45,13 +48,17 @@ function renderPrograms(programs) {
                     <strong>Campus:</strong> ${program.location.campus.title}
                 </div>
                 <div class="location">
-                    <strong>Location: </strong>${program.room}, ${program.location.title},  ${program.location.address}, ${program.location.postcode}
+                    <strong>Location: </strong>${program.room}, ${
+        program.location.title
+      },  ${program.location.address}, ${program.location.postcode}
                 </div>
                 <div class="date">
                     <strong>Date:</strong> ${formatDate(program.start_time)}
                 </div>
                 <div class="time">
-                    <strong>Time:</strong> ${formatTime(program.start_time)} - ${formatTime(program.end_time)}
+                    <strong>Time:</strong> ${formatTime(
+                      program.start_time
+                    )} - ${formatTime(program.end_time)}
                 </div>
             </section>
             
@@ -67,8 +74,8 @@ function renderPrograms(programs) {
             </section>
         </div>
     `
-        )
-        .join("");
+    )
+    .join("");
 }
 
 /**
@@ -76,21 +83,22 @@ function renderPrograms(programs) {
  * @param topics single topic or a Map collection of topics
  */
 function renderTopic(topics) {
+  let topicArray;
 
-    let topicArray;
+  if (topics instanceof Map) {
+    topicArray = Array.from(topics.values());
+  } else if (Array.isArray(topics)) {
+    topicArray = topics;
+  } else if (typeof topics === "object" && topics !== null) {
+    topicArray = [topics];
+  } else {
+    console.warn("Invalid topics input:", topics);
+    return;
+  }
 
-    if (topics instanceof Map) {
-        topicArray = Array.from(topics.values());
-    } else if (Array.isArray(topics)) {
-        topicArray = topics;
-    } else if (typeof topics === "object" && topics !== null) {
-        topicArray = [topics];
-    } else {
-        console.warn("Invalid topics input:", topics);
-        return;
-    }
-
-    const topicHtml = topicArray.map((topic) => `
+  const topicHtml = topicArray
+    .map(
+      (topic) => `
         <section class="topic">
             <div class="topic-header">
                 <div>
@@ -103,87 +111,88 @@ function renderTopic(topics) {
                 ${renderPrograms(topic.programs)}
             </div>
         </section>
-    `).join("");
+    `
+    )
+    .join("");
 
-    // clean old html and insert new html
-    topicContainer.innerHTML = "";
-    topicContainer.insertAdjacentHTML("beforeend", topicHtml);
-
+  // clean old html and insert new html
+  topicContainer.innerHTML = "";
+  topicContainer.insertAdjacentHTML("beforeend", topicHtml);
 }
 
 function renderMainHeader(data) {
-
-    const htmlElement = `
+  const htmlElement = `
        <h1>
             ${data.description}
        </h1>
        <h2>
-            🕰️ <i>${formatTime(data.start_time)}</i> - <i>${formatTime(data.end_time)}</i>
+            🕰️ <i>${formatTime(data.start_time)}</i> - <i>${formatTime(
+    data.end_time
+  )}</i>
        </h2>
-    `
-    document.querySelector(".main-header").insertAdjacentHTML("beforeend", htmlElement);
+    `;
+  document
+    .querySelector(".main-header")
+    .insertAdjacentHTML("beforeend", htmlElement);
 }
 
-function addTopicsNames() {
+function addTopicsNamesToSelector() {
+  // using fragement store topic options
+  const fragment = document.createDocumentFragment();
 
-    // using fragement store topic options
-    const fragment = document.createDocumentFragment();
+  topicNames.forEach((name) => {
+    const option = document.createElement("option");
+    option.value = name;
+    option.textContent = name;
+    fragment.appendChild(option);
+  });
 
-    topicNames.forEach(name => {
-        const option = document.createElement("option");
-        option.value = name;
-        option.textContent = name;
-        fragment.appendChild(option);
-    });
-
-    select.appendChild(fragment);
+  select.appendChild(fragment);
 }
 
 function addDropdownMenuEventListen() {
-    select.addEventListener("change", (event) => {
-        const selected = event.target.value;
-        if (selected === "all") {
-            renderTopic(topicMap);
-        } else {
-            const topic = topicMap.get(selected);
-            renderTopic(topic);
-        }
-    });
+  select.addEventListener("change", (event) => {
+    const selected = event.target.value;
+    if (selected === "all") {
+      renderTopic(topicMap);
+    } else {
+      const topic = topicMap.get(selected);
+      renderTopic(topic);
+    }
+  });
 }
 
 async function loadData() {
-
-    try {
-        const response = await fetch("OpenDay.json");
-        if (!response.ok) {
-            //TODO leave for future when using HTTP request
-            throw new Error('');
-        }
-        const data = await response.json();
-        renderMainHeader(data);
-        data.topics.forEach(element => {
-            // Store topic names into list for add to drop menu later
-            topicNames.push(element.name);
-            // Using Map collection for constant access time
-            topicMap.set(element.name, element);
-        });
-    } catch (error) {
-        //TODO leave for future when using HTTP request
-        throw new Error(error);
+  try {
+    const response = await fetch("OpenDay.json");
+    if (!response.ok) {
+      //TODO leave for future when using HTTP request
+      throw new Error("");
     }
+    const data = await response.json();
+    renderMainHeader(data);
+    data.topics.forEach((element) => {
+      // Store topic names into list for add to drop menu later
+      topicNames.push(element.name);
+      // Using Map collection for constant access time
+      topicMap.set(element.name, element);
+    });
+  } catch (error) {
+    //TODO leave for future when using HTTP request
+    throw new Error(error);
+  }
 }
 
 async function init() {
-    try {
-        await loadData();
-        renderTopic(topicMap);
-        addTopicsNames();
-        addDropdownMenuEventListen();
-    } catch (error) {
-        //TODO leave for future when using HTTP request
-        console.error(error);
-    }
+  try {
+    await loadData();
+    renderTopic(topicMap);
+    addTopicsNames();
+    addDropdownMenuEventListen();
+  } catch (error) {
+    //TODO leave for future when using HTTP request
+    console.error(error);
+  }
 }
 
 init();
-
